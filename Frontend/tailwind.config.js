d91841a505d15fbac57ca06192989c20f7932f38
AmoabaKelvin <kel.amoaba@gmail.com>
2023-08-12 12:35:21 +0000

    set up nextui

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,10 +1,12 @@
+const {nextui} = require('@nextui-org/react')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -20,5 +22,6 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
