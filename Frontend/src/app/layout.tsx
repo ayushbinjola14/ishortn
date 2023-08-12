dc88978b06e1b49d7b257165f62c4fdda506b9f6
AmoabaKelvin <kel.amoaba@gmail.com>
2023-08-12 12:36:00 +0000

    add NextUI provider and reformat code

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -1,24 +1,27 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter, Open_Sans } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+import { Providers } from './providers';
+
+const inter = Inter({ subsets: ['latin'] });
+const openSans = Open_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "iShortn",
+  title: 'iShortn',
   description: `Power up your links with our AI-driven analytics, advanced URL
   shortening, and dynamic QR code creation and boost engagement
   results like never before. Unleash the power of your links today!`,
   authors: [
     {
-      name: "Kelvin Amoaba",
-      url: "https://www.kelvinamoaba.live",
+      name: 'Kelvin Amoaba',
+      url: 'https://www.kelvinamoaba.live',
     },
   ],
-  applicationName: "iShortn",
-  creator: "Kelvin Amoaba",
-  robots: "index, follow",
-  category: "Technology",
+  applicationName: 'iShortn',
+  creator: 'Kelvin Amoaba',
+  robots: 'index, follow',
+  category: 'Technology',
 };
 
 export default function RootLayout({
@@ -27,8 +30,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark">
+      <body className={openSans.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
